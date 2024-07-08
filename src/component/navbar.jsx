43216d08510dbce50462f2817007786a0b99101a
SeungMin915 <86399828+SeungMin915@users.noreply.github.com>
2024-07-08 16:14:17 +0900

    Update navbar.jsx

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -44,6 +44,11 @@ const Navbar = () => {
               </Link>
             </li>
             <li className="nav-item">
+              <Link className="nav-link" to="/summary">
+                Summary
+              </Link>
+            </li>
+            <li className="nav-item">
               <Link className="nav-link" to="/draftai">
                 DraftAI
               </Link>
@@ -76,6 +81,11 @@ const Navbar = () => {
             <li className="nav-item">
               <Link className="nav-link" to="/home" onClick={toggleSidebar}>
                 Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/summary" onClick={toggleSidebar}>
+                Summary
               </Link>
             </li>
             <li className="nav-item">
