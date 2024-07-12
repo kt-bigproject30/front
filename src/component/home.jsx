5c294690e3a84cbcc34313c3f03008e8fc9fa5a8
SeungMin915 <galaxy13570@gmail.com>
2024-07-12 14:41:52 +0900

    메인페이지, 메뉴얼 페이지

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -1,14 +1,90 @@
-import React from "react";
+// import React from "react";
+// import "../css/home.css";
+// import jasingamImage from "../img/jasingam.jpg";
+
+// const Home = () => {
+//   return (
+//     <div className="home-container">
+//       <h1>Jasingam</h1>
+//       <img src={jasingamImage} alt="Jasingam" className="start-image" />
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+
+import React, { useEffect, useRef, useState } from "react";
 import "../css/home.css";
 import jasingamImage from "../img/jasingam.jpg";
 
 const Home = () => {
+  const imageRef = useRef(null);
+  const [imageHeight, setImageHeight] = useState(0);
+  const [activeButton, setActiveButton] = useState(0);
+
+  const updateImageHeight = () => {
+    if (imageRef.current) {
+      setImageHeight(imageRef.current.clientHeight);
+    }
+  };
+
+  useEffect(() => {
+    updateImageHeight(); // Initial update on component mount
+
+    window.addEventListener("resize", updateImageHeight);
+    return () => {
+      window.removeEventListener("resize", updateImageHeight);
+    };
+  }, []);
+
+  const moveButtonClick = () => {
+    
+    window.location.href = './usepage';
+  };
+
   return (
     <div className="home-container">
       <h1>Jasingam</h1>
-      <img src={jasingamImage} alt="Jasingam" className="start-image" />
+      <div className="content">
+        <div className="image-wrapper">
+          <img
+            src={jasingamImage}
+            alt="Image of Jasingam"
+            className="start-image"
+            ref={imageRef}
+            onLoad={updateImageHeight}
+          />
+          <div className="controls">
+            <button className="arrow-button">{"<"}</button>
+            {[...Array(5)].map((_, index) => (
+              <button
+                key={index}
+                className={`circle-button ${activeButton === index ? "active" : ""}`}
+                onClick={() => setActiveButton(index)}
+              ></button>
+            ))}
+            <button className="arrow-button">{">"}</button>
+          </div>
+        </div>
+        <div className="side-content">
+          <div
+            className="side-box"
+            style={{ height: imageHeight / 2 }}
+          >
+            공지사항
+          </div>
+          <button
+            className="side-box" onClick={moveButtonClick}
+            style={{ height: imageHeight / 2 }}
+          >
+            사용 방법
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Home;
+
---
+++ src/component/usepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/usepage.css";
+
+const Usepage = () => {
+  return (
+    <div className="usepage-container">
+      <h1>사용법 페이지</h1>
+    </div>
+  );
+};
+
+export default Usepage;
